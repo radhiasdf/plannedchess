54b1cd25b988721255f054af5bb08ed7169fa3ed
radhiasdf <radhityarr@gmail.com>
2023-10-24 15:04:22 +1300

    improve drawing piece system

--- assets.py
+++ assets.py
@@ -1,7 +1,20 @@
 import pygame
 pieceImgs = {}
-for name in ("K","k","Q","q","R", "r", "B","b", "N", "n", "P", "p"):
+for piece_libname, svg_name in (("K", "kw"),
+								("k","kb"),
+								("Q","qw"),
+								("q","qb"),
+								("R","rw"),
+								("r","rb"),
+								("B","bw"),
+								("b","bb"),
+								("N","nw"),
+								("n","nb"),
+								("P","pw"),
+								("p","pb")):
 	try:
-		pieceImgs[name] = pygame.image.load(f"assets/{name}.svg")
+		pieceImgs[piece_libname] = pygame.image.load(f"assets/{svg_name}.svg")
 	except FileNotFoundError:
 		print("Piece image not found")
+
+# By Cburnett - Own work, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=1499806
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import chess
 import pygame as pg
 from assets import pieceImgs
 
+pg.init()
+font = pg.font.Font(None, 36)
 
 def scaleImages(scale):  # Vector2 scale  # returns dict
 	pieceTransforms = {}
@@ -11,16 +13,18 @@ def scaleImages(scale):  # Vector2 scale  # returns dict
 
 
 def coordToPixel(coord):
-	return coord[0] * square_size[0], coord[1] * square_size[1]
+	return int(coord[0] * square_size[0]), int(coord[1] * square_size[1])
 
 
 def draw_piece(piece_key, coord):
-	if board.piece_at(row * 8 + col) is None:
-		return
+	if str(piece_key) == "None":
+		print("it will return")
 	try:
-		dest = coordToPixel(coord)
-		screen.blit(pieceTransforms[str(piece_key)], dest)
+		print(str(piece_key))
+		print(coordToPixel(coord))
+		screen.blit(pieceTransforms[str(piece_key)], coordToPixel(coord))
 	except KeyError:
+		print("keyerror")
 		pass
 
 
@@ -47,14 +51,24 @@ running = True
 # game loop
 while running:
 	screen.fill(background_colour)
-	for row in range(8):
-		for col in range(8):
-			if (row+col+1) % 2 == 0:
+	for row in range(0,8):
+		for col in range(0, 8):
+			if (row+col) % 2 == 0:
 				pg.draw.rect(screen, "white", (row*square_size.x, col*square_size.y, square_size.x, square_size.y))
-			try:
-				draw_piece(str(board.piece_at(row*8+col)), (col, row))
-			except:
-				pass
+	for row in range(0,8):
+		for col in range(0, 8):
+			draw_piece(str(board.piece_at(63-(row*8+col))), (col, row))
+			print(63-(row*8+col))
+			print(col, row)
+
+
+	text_surface = font.render(str(pg.mouse.get_pos()), True, (0, 0, 0))  # Text, antialias, color
+	text_rect = text_surface.get_rect()
+	text_rect.center = (screen_dim // 2)  # Center the text
+
+	# Blit the text onto the screen
+	screen.blit(text_surface, text_rect)
+
 	pg.display.update()
 
 	# for loop through the event queue
